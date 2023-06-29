f550cc1be016e53bd2ae9ddba452a46573ddb0fe
janskrt <138119712+janskrt@users.noreply.github.com>
2023-06-29 19:25:04 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.css";
-import "react-toastify/dist/ReactToastify.css";
-import './index.css';
-import App from './App';
-//import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Button, FormGroup, Input, Row, Col} from "reactstrap";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const initialUser ={email:"", password: "", username: ""}
+const Registration = () => {
+    const [user, setUser] = useState(initialUser);
+    const navigate = useNavigate();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+    const signUp = async () => {
+        try {
+            const url = `http://localhost:1337/api/auth/local/register`;
+            if (user.username && user.email && user.password) {
+                const res = await axios.post(url,user);
+                if (res){
+                    setUser(initialUser);
+                    navigate("/login");
+                }
+            }
+        } catch (error) {
+            toast.error(error.message, {
+                hideProgressBar: true,
+            });
+        }
+    };
+
+    const handleUserChange = ({target}) => {
+        const {name, value} =  target;
+        setUser((currentUser) => ({
+        ...currentUser,
+        [name]: value,        
+        }));
+    };
+
+    return (
+    <Row className="register">
+        <Col sm ='12' md={{size: 4, offset: 4}}>
+        <   div>
+            <h2>Sign up:</h2>
+            <FormGroup>
+            <Input 
+                    type="text" 
+                    name="username" 
+                    value={user.username} 
+                    onChange={handleUserChange} 
+                    placeholder="Enter your full name"
+            />
+        </FormGroup>
+        <FormGroup>
+            <Input 
+                    type="email" 
+                    name="email" 
+                    value={user.email} 
+                    onChange={handleUserChange} 
+                    placeholder="Enter your email"
+            />
+        </FormGroup>
+        <FormGroup>
+            <Input 
+                    type="password" 
+                    name="password" 
+                    value={user.password} 
+                    onChange={handleUserChange} 
+                    placeholder="Enter password"
+            />
+        </FormGroup>
+        <Button color="primary" onClick={signUp}>
+            Sign up
+            </Button>
+             </div>
+        </Col>
+    </Row>
+    ); 
+};
+
+export default Registration;
